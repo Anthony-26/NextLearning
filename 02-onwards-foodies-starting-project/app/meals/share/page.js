0b85e4998c69f04190df9164a8cc5ee9f0db72fa
Anthony <anthony.rousseau26@gmail.com>
2024-02-12 15:19:32 +0100

    Using cache revalidation

--- 02-onwards-foodies-starting-project/app/meals/share/page.js
+++ 02-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,12 +1,13 @@
+'use client';
 
-
+import { useFormState } from 'react-dom';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
-//   const status = useFormStatus();
+  const [state, formAction] = useFormState(shareMeal, { message: null });
 
   return (
     <>
@@ -17,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -46,6 +47,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
