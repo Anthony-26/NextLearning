f11e303e22bcbf59623c660a583c07cc603e5bb3
Anthony <anthony.rousseau26@gmail.com>
2024-02-28 21:36:40 +0100

    Adding navbar basic feature

--- 06-car-showcase/components/index.ts
+++ 06-car-showcase/components/index.ts
@@ -1,4 +1,6 @@
 import Hero from "./Hero";
 import CustomButton from "./custom-button";
+import Navbar from "./navbar";
+import Footer from "./footer";
 
-export { Hero, CustomButton }
+export { Hero, CustomButton, Navbar, Footer }
