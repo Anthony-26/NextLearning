d3d4f37f21a91d5b76a0c8c97db4158cc4c9737b
Anthony <anthony.rousseau26@gmail.com>
2024-02-29 22:20:06 +0100

    Setting up homepage components

--- 06-car-showcase/components/index.ts
+++ 06-car-showcase/components/index.ts
@@ -1,6 +1,6 @@
 import Hero from "./hero";
 import CustomButton from "./custom-button";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Footer from "./footer";
 
 export { Hero, CustomButton, Navbar, Footer }
